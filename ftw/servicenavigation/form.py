c313db6819cf34683a7e97eada91611c54ffe2ba
Elio Schmutz <e.schmutz@4teamwork.ch>
2019-08-19 15:09:13 +0200

    Validate that there is either an external or an internal link but not both.

--- ftw/servicenavigation/form.py
+++ ftw/servicenavigation/form.py
@@ -23,6 +23,8 @@ from zope.component import getUtility
 from zope.component.hooks import getSite
 from zope.interface import alsoProvides
 from zope.interface import implements
+from zope.interface import Invalid
+from zope.interface import invariant
 from zope.intid.interfaces import IIntIds
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary
@@ -114,6 +116,13 @@ class IServiceNavigationSchema(model.Schema):
         default=True,
         missing_value=True,
     )
+
+    @invariant
+    def link_validator(data):
+        for link in data.links:
+            if link.get('internal_link', None) and link.get('external_url', None):
+                raise Invalid(_('error_link_validator',
+                                u'Choose between an external or an internal link.'))
 
 
 class ServiceNavigationConfig(object):
