873ebbda345e3fe79c816246a2410631d6c2efe9
Matthias Osswald <m@osswald.li>
2020-06-23 15:01:36 +0200

    Fix javascript (AMD pattern & select2 check).

--- ftw/servicenavigation/resources/js/service-edit-form.js
+++ ftw/servicenavigation/resources/js/service-edit-form.js
@@ -1,44 +1,48 @@
-$(function() {
-
-  function fontAwesomeIcons(state){
-      if (!state.id) { return state.text; }
-
-      var template = $("<span class='fa-icon fa-" + state.element.value + "'></span><span>"+ state.text + "</span>");
-      return template;
-  }
+require([
+  'jquery',
+], function($,){
+  $(document).ready(function() {
 
 
-  var initIconSelection = function(){
-    var element = $(".datagridwidget-body tr:not(.datagridwidget-empty-row) select.select-widget");
+    function fontAwesomeIcons(state){
+        if (!state.id) { return state.text; }
 
-    element.select2({
-        width: "100%",
-        templateResult: fontAwesomeIcons,
-        templateSelection: fontAwesomeIcons
-    });
-
-    // Never use inline validation
-    $(".z3cformInlineValidation").removeClass("z3cformInlineValidation");
-  };
-
-
-  if ($("#service-edit-form").length === 1){
-
-    if ($.select2 !== undefined) {
-      initIconSelection();
-
-    } else {
-      var base_plugin_url = portal_url + "/++resource++servicenavigation/select2-4.0.0/dist";
-      var select2_plugin = base_plugin_url + "/js/select2.min.js";
-
-      $.getScript(select2_plugin, function(){
-        $("head").append( $('<link rel="stylesheet" type="text/css" />').attr("href", base_plugin_url + "/css/select2.min.css") );
-        initIconSelection();
-      });
+        var template = $("<span class='fa-icon fa-" + state.element.value + "'></span><span>"+ state.text + "</span>");
+        return template;
     }
 
-    $(document).on("change", ".datagridwidget-body", initIconSelection);
 
-  }
+    var initIconSelection = function(){
+      var element = $(".datagridwidget-body tr:not(.datagridwidget-empty-row) select.select-widget");
 
+      element.select2({
+          width: "100%",
+          templateResult: fontAwesomeIcons,
+          templateSelection: fontAwesomeIcons
+      });
+
+      // Never use inline validation
+      $(".z3cformInlineValidation").removeClass("z3cformInlineValidation");
+    };
+
+
+    if ($("#service-edit-form").length === 1){
+      // in Plone 5 select2 comes with plone
+      if ($.fn.select2) {
+        initIconSelection();
+      } else {
+        var base_plugin_url = portal_url + "/++resource++servicenavigation/select2-4.0.0/dist";
+        var select2_plugin = base_plugin_url + "/js/select2.min.js";
+
+        $.getScript(select2_plugin, function(){
+          $("head").append(
+            $('<link rel="stylesheet" type="text/css" />').attr(
+              "href", base_plugin_url + "/css/select2.min.css") );
+          initIconSelection();
+        });
+      }
+    }
+
+
+  });
 });
--- ftw/servicenavigation/resources/js/servicenavigation-compiled.js
+++ ftw/servicenavigation/resources/js/servicenavigation-compiled.js
@@ -1,1 +1,55 @@
-$(function(){function e(e){return e.id?$("<span class='fa-icon fa-"+e.element.value+"'></span><span>"+e.text+"</span>"):e.text}var t=function(){$(".datagridwidget-body tr:not(.datagridwidget-empty-row) select.select-widget").select2({width:"100%",templateResult:e,templateSelection:e}),$(".z3cformInlineValidation").removeClass("z3cformInlineValidation")};if(1===$("#service-edit-form").length){if(void 0!==$.select2)t();else{var i=portal_url+"/++resource++servicenavigation/select2-4.0.0/dist",n=i+"/js/select2.min.js";$.getScript(n,function(){$("head").append($('<link rel="stylesheet" type="text/css" />').attr("href",i+"/css/select2.min.css")),t()})}$(document).on("change",".datagridwidget-body",t)}}),define("service-edit-form",function(){}),require(["service-edit-form"],function(e){}),define("main",function(){});
+require([
+  'jquery',
+], function($,){
+  $(document).ready(function() {
+
+
+    function fontAwesomeIcons(state){
+        if (!state.id) { return state.text; }
+
+        var template = $("<span class='fa-icon fa-" + state.element.value + "'></span><span>"+ state.text + "</span>");
+        return template;
+    }
+
+
+    var initIconSelection = function(){
+      var element = $(".datagridwidget-body tr:not(.datagridwidget-empty-row) select.select-widget");
+
+      element.select2({
+          width: "100%",
+          templateResult: fontAwesomeIcons,
+          templateSelection: fontAwesomeIcons
+      });
+
+      // Never use inline validation
+      $(".z3cformInlineValidation").removeClass("z3cformInlineValidation");
+    };
+
+
+    if ($("#service-edit-form").length === 1){
+      // in Plone 5 select2 comes with plone
+      if ($.fn.select2) {
+        initIconSelection();
+      } else {
+        var base_plugin_url = portal_url + "/++resource++servicenavigation/select2-4.0.0/dist";
+        var select2_plugin = base_plugin_url + "/js/select2.min.js";
+
+        $.getScript(select2_plugin, function(){
+          $("head").append(
+            $('<link rel="stylesheet" type="text/css" />').attr(
+              "href", base_plugin_url + "/css/select2.min.css") );
+          initIconSelection();
+        });
+      }
+    }
+
+
+  });
+});
+
+define("service-edit-form", function(){});
+
+require(['service-edit-form'], function (service_edit_form) {});
+
+define("main", function(){});
+
